e92c8da2b7223f880e8e87eedb4fec747420d92b
izzeddincelik <izzeddinmupe@gmail.com>
2023-05-31 22:48:10 +0300

    rule set were numeralized

--- one-click-install.sh
+++ one-click-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo
-echo "GitHub repo clone'lanıyor..."
+echo "1. GitHub repo clone'lanıyor..."
 sleep 2
 git clone https://github.com/roofstacks/case-study-pool.git
 cd case-study-pool/infrastructure-developer/mini-cluster/sample-app
@@ -10,7 +10,7 @@ cd case-study-pool/infrastructure-developer/mini-cluster/sample-app
 mv sample-app.csproj sample_app.csproj
 
 echo
-echo "Dockerfile oluşturuluyor..."
+echo "2. Dockerfile oluşturuluyor..."
 cat <<EOF > Dockerfile
 FROM mcr.microsoft.com/dotnet/sdk:5.0 AS build-env
 WORKDIR /app
@@ -33,16 +33,16 @@ EXPOSE 443
 ENTRYPOINT ["dotnet", "sample_app.dll"]
 EOF
 
-echo "Docker imajı oluşturuluyor..."
+echo "3. Docker imajı oluşturuluyor..."
 echo "docker build -t izzeddincelik/sample_app ."
 sleep 2
 echo
-echo "Image DockerHub'a  push ediliyor..."
+echo "4. Image DockerHub'a  push ediliyor..."
 echo "docker push izzeddincelik/sample_app"
 sleep 2
 echo
 
-echo "Kubernetes definition file oluşturuluyor..."
+echo "5. Kubernetes definition file oluşturuluyor..."
 echo
 cat <<EOF > k8s-goart-definition-file.yml
 apiVersion: apps/v1
@@ -95,22 +95,22 @@ spec:
                   number: 80
 EOF
 
-echo "Kubernetes Deployment - SVC - Ingress oluşturuluyor..."
+echo "6. Kubernetes Deployment - SVC - Ingress oluşturuluyor..."
 sleep 2
 kubectl apply -f k8s-goart-definition-file.yml
 
 # Uygulama erişimini kontrol etme
 echo
-echo "Uygulama erişimi için Minikube Tunnel'in açık olduğundan emin olunuz..."
+echo "7. Uygulama erişimi için Minikube Tunnel'in açık olduğundan emin olunuz..."
 sleep 4
 
 echo
-echo "IP adresi ve port bilgileri alınıyor..."
+echo "8. IP adresi ve port bilgileri alınıyor..."
 IP=$(kubectl get svc sampleappservice -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 PORT=$(kubectl get svc sampleappservice -o jsonpath='{.spec.ports[0].port}')
 
 # Uygulamayı test et
 echo
-echo "curl http://$IP:$PORT/WeatherForecast"
+echo "9. curl http://$IP:$PORT/WeatherForecast"
 echo
 curl http://$IP:$PORT/WeatherForecast
